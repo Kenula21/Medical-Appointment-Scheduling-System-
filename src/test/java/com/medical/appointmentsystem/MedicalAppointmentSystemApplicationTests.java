f7f9e2798e99e7832a0df9c86d4c64186133af05
IT24102432 <IT24102432@my.sliit.lk>
2025-05-13 16:27:23 +0530

    Create MedicalAppointmentSystemApplicationTests.java

---
+++ src/test/java/com/medical/appointmentsystem/MedicalAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.appointmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
